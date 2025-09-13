ca4ff0a7db5e7cae417f9a9eb4ce336373d7f83c
JuanTorres-hash <torresromerojuanjose3@gmail.com>
2025-09-13 08:40:28 -0500

    informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,13 @@
+<?php
+$Nombre = $_POST['Nombre'];
+$Edad = $_POST['Edad'];
+$Direccion = $_POST['Direccion'];
+$Salario = $_POST['Salario'];
+$CorreoElectronico = $_POST['CorreoElectronico'];
+$Cargo = $_POST['Cargo'];
 
+echo "<h2>Información Recibida</h2>";
+echo "El empleado $Nombre tiene $Edad años.<br>";
+echo "Vive en $Direccion y su salario es: $Salario.<br>";
+echo "Su correo electronico es $CorreoElectronico y su cargo es $Cargo.";
+?>
